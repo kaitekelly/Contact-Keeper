1d2de9a0a8f356c26f13eb006892bba4e282bbf7
kaitekelly <murphy.kaite@gmail.com>
2021-05-18 16:40:14 -0700

    jwt token

--- routes/auth.js
+++ routes/auth.js
@@ -20,7 +20,42 @@ async (req, res) => {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const { email, password }
+    const { email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        if(!user) {
+          return res.status(400).json({ msg: 'Invalid Credentials' });  
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch) {
+            return res.status(400).json({ msg: 'Invalid Credentials'})
+        }
+
+        const payload = {
+            user: {
+              id: user.id,
+            },
+          };
+    
+          jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            {
+              expiresIn: 360000,
+            },
+            (err, token) => {
+              if (err) throw err;
+              res.json({ token });
+            }
+          );
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 // @route   POST api/auth
