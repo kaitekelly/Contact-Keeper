28687341255c12a61a37c83fe62754e9a438570f
kaitekelly <murphy.kaite@gmail.com>
2021-05-17 22:04:12 -0700

    remove MOngo U R I

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 const router = express.Router();
 
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Private  
-router.get('/', (req, res) => {
-    res.send('Get logged in user');
+router.get('/', [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+], 
+async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password }
 });
 
 // @route   POST api/auth
