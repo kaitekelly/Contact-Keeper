944f7178f3b47b5133e67ce86107b411289ef7e4
kaitekelly <murphy.kaite@gmail.com>
2021-05-25 19:01:26 -0700

    delete contact functionality

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -48,6 +48,10 @@ const ContactState = props => {
     };
 
     //Delete Contact
+    const deleteContact = id => {
+        // contact.id = uuid.v4;
+        dispatch({ type: DELETE_CONTACT, payload: id });
+    };
 
     //Set Current Contact
 
@@ -63,7 +67,8 @@ const ContactState = props => {
         <ContactContext.Provider
         value={{
             contacts: state.contacts,
-            addContact
+            addContact,
+            deleteContact
         }}
         >
             {props.children}
